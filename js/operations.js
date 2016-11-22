10d8e98e4c0299ad8cfc899ffaa5aa5e61d5ec60
Josh <schwal10@msu.edu>
2016-11-22 00:35:26 -0500

    More operations!
    Added four more operation class: min, max, range, and mod. Will
    implement them in operation array when button/setting options are
    available.

--- js/operations.js
+++ js/operations.js
@@ -111,3 +111,99 @@ Division.prototype.operation = function(arrayOfNumbers) {
 	}
 	return resultOfOperation
 }
+
+// The class for min
+function Minimum() {
+	this.minCells = 2 // Can operate on a minimum of 2 cells
+	this.maxCells = 3 // Can operate on a maximum of 3 cells
+	this.symbol = 'min'
+}
+
+// The operation function for minimum
+Minimum.prototype.operation = function(arrayOfNumbers) {
+	// Verify that array length is within the size constraints, if not return 0 meaning the operation failed
+	if(arrayOfNumbers.length > this.maxCells || arrayOfNumbers.length < this.minCells) {
+		return false
+	}
+	
+	var resultOfOperation = arrayOfNumbers[0]
+	for(var i = 1; i < arrayOfNumbers.length; i++) {
+		if(resultOfOperation > arrayOfNumbers[i]) {
+			resultOfOperation = arrayOfNumbers[i]
+		}
+	}
+	return resultOfOperation
+}
+
+// The class for max
+function Maximum() {
+	this.minCells = 2 // Can operate on a minimum of 2 cells
+	this.maxCells = 3 // Can operate on a maximum of 3 cells
+	this.symbol = 'max'
+}
+
+// The operation function for maximum
+Maximum.prototype.operation = function(arrayOfNumbers) {
+	// Verify that array length is within the size constraints, if not return 0 meaning the operation failed
+	if(arrayOfNumbers.length > this.maxCells || arrayOfNumbers.length < this.minCells) {
+		return false
+	}
+	
+	var resultOfOperation = arrayOfNumbers[0]
+	for(var i = 1; i < arrayOfNumbers.length; i++) {
+		if(resultOfOperation < arrayOfNumbers[i]) {
+			resultOfOperation = arrayOfNumbers[i]
+		}
+	}
+	return resultOfOperation
+}
+
+// The class for range
+function Range() {
+	this.minCells = 2 // Can operate on a minimum of 2 cells
+	this.maxCells = undefined // No max number of cells it can operate on
+	this.symbol = 'range'
+}
+
+// The operation function for range
+Range.prototype.operation = function(arrayOfNumbers) {
+	// Verify that array length is within the size constraints, if not return 0 meaning the operation failed
+	if(arrayOfNumbers.length > this.maxCells || arrayOfNumbers.length < this.minCells) {
+		return false
+	}
+	
+	var min = arrayOfNumbers[0]
+	var max = arrayOfNumbers[0]
+	for(var i = 1; i < arrayOfNumbers.length; i++) {
+		if(max < arrayOfNumbers[i]) {
+			max = arrayOfNumbers[i]
+		} else if(min > arrayOfNumbers[i]) {
+			min = arrayOfNumbers[i]
+		}
+	}
+	return max-min
+}
+
+// The class for mod
+function Mod() {
+	this.minCells = 2 // Can operate on a minimum of 2 cells
+	this.maxCells = 2 // Can operate on a maximum of 2 cells
+	this.symbol = '%'
+}
+
+// The operation function for mod
+Mod.prototype.operation = function(arrayOfNumbers) {
+	// Verify that array length is within the size constraints, if not return 0 meaning the operation failed
+	if(arrayOfNumbers.length > this.maxCells || arrayOfNumbers.length < this.minCells) {
+		return false
+	}
+	
+	// Should only have two numbers, take larger % smaller
+	var resultOfOperation = false;
+	if(arrayOfNumbers[0] > arrayOfNumbers[1]) {
+		resultOfOperation = arrayOfNumbers[0] % arrayOfNumbers[1]
+	} else {
+		resultOfOperation = arrayOfNumbers[1] % arrayOfNumbers[0]
+	}
+	return resultOfOperation
+}
